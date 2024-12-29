675c8d075c10017a664bdfeb9773b6f036bd90d5
Mahdi111191 <mahdilove7799@gmail.com>
2024-12-29 08:57:34 +0330

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="calculator">
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.calculator {
+  max-width: 260px;
+  margin: 0 auto;
+  border: 2px solid #ccc;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+</style>
